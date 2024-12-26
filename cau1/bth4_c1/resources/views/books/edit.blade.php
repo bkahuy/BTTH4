95cfcd3a66b339245ceb73ee9caa47cd54df18b0
Bùi Khắc Huy <buikhachuy003@gmail.com>
2024-12-27 01:09:22 +0700

    khac huy hoan thanh book

--- cau1/bth4_c1/resources/views/books/edit.blade.php
+++ cau1/bth4_c1/resources/views/books/edit.blade.php
@@ -1,1 +1,35 @@
-<?php
+@extends('welcome')
+@section('title', 'Form sua sach')
+
+@section('main')
+    <div class="container">
+        <h1>Sua sach</h1>
+
+        <form action="{{ route('books.update', $books) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="name">Ten sach:</label>
+                <input type="text" class="form-control" id="name" name="name" value="{{ $books->name }}">
+            </div>
+            <div class="form-group">
+                <label for="author">Tac gia:</label>
+                <textarea class="form-control" id="author" name="author" required>{{ $books->author }}</textarea>
+            </div>
+            <div class="form-group">
+                <label for="category">The loai:</label>
+                <textarea class="form-control" id="category" name="category">{{ $books->category }}</textarea>
+            </div>
+            <div class="form-group">
+                <label for="year">Nam xuat ban:</label>
+                <textarea class="form-control" id="year" name="year">{{ $books->year }}</textarea>
+            </div>
+            <div class="form-group">
+                <label for="quantity">So luong ton:</label>
+                <textarea class="form-control" id="quantity" name="quantity">{{ $books->quantity }}</textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Lưu</button>
+        </form>
+
+    </div>
+@endsection
--- cau1/bth4_c1/resources/views/books/index.blade.php
+++ cau1/bth4_c1/resources/views/books/index.blade.php
@@ -8,7 +8,6 @@
     <table class="table">
         <thead>
         <tr>
-            <th scope="col">id</th>
             <th scope="col">Tên sách</th>
             <th scope="col">Tác giả</th>
             <th scope="col">Thể loại sách</th>
@@ -20,15 +19,39 @@
         <tbody>
         @foreach($books as $book)
         <tr>
-            <th scope="row">{{ $book->id }}</th>
             <td>{{ $book->name }}</td>
             <td>{{ $book->author }}</td>
             <td>{{ $book->category }}</td>
             <td>{{ $book->year }}</td>
             <td>{{ $book->quantity }}</td>
             <td>
-                <a href=""><i class="bi bi-pencil me-3"></i></a>
-                <a href=""><i class="bi bi-trash3 ms-3"></i></a>
+                <a href="{{ route( 'books.edit', $book->id) }}"><i class="bi bi-pencil me-3"></i></a>
+                <!-- Button trigger modal -->
+                <a class="bi bi-trash3 ms-3" data-bs-toggle="modal" data-bs-target="#{{$book->id}}"></a>
+
+                <!-- Modal -->
+                <div class="modal fade" id="{{$book->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">XOA BAI VIET {{$book->id}}</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                ban co chac chan muon xoa {{$book->id}} khong
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">huy</button>
+                                <form action="{{ route('books.destroy', $book->id) }}" method="POST" style="display: inline-block;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">xac nhan</button>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </td>
         </tr>
         @endforeach
